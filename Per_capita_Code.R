bd47450864c5a7d329b86379a748ba6aa7e3c46e
schmidsp-bc <124622240+schmidsp-bc@users.noreply.github.com>
2023-02-26 18:38:30 -0500

    Update Per_capita_Code.R

--- Per_capita_Code.R
+++ Per_capita_Code.R
@@ -11,26 +11,29 @@ zipcodes <- read.csv("C:\\Users\\sarah\\Documents\\Econometrics\\final_project\\
 #What about per capita? Does it depend on liquor category?
 
 ## Total Sales
-cities.total.sales   <- cities %>% select(city, income, sale.dollars, sale.volume)
+cities.total.sales   <- cities %>% select(city, income, sale.dollars, sale.volume, population)
 cities.total.sales   <- cities.total.sales %>%
   group_by(city) %>%
   summarize(total.sales.dollars = sum(sale.dollars), 
             total.sales.volume  = sum(sale.volume), 
-            income              = median(income))
+            income              = median(income),
+            population          = median(population))
 
-counties.total.sales <- counties %>% select(county, income, sale.dollars, sale.volume)
+counties.total.sales <- counties %>% select(county, income, sale.dollars, sale.volume, population)
 counties.total.sales <- counties.total.sales %>%
   group_by(county) %>%
   summarize(total.sales.dollars = sum(sale.dollars),
             total.sales.volume  = sum(sale.volume), 
-            income              = median(income))
+            income              = median(income),
+            population          = median(population))
 
-zipcodes.total.sales <- zipcodes %>% select(zipcode, income, sale.dollars, sale.volume)
+zipcodes.total.sales <- zipcodes %>% select(zipcode, income, sale.dollars, sale.volume, population)
 zipcodes.total.sales <- zipcodes.total.sales %>%
   group_by(zipcode) %>%
   summarize(total.sales.dollars = sum(sale.dollars), 
             total.sales.volume  = sum(sale.volume), 
-            income              = median(income))
+            income              = median(income),
+            population          = median(population))
 
 ## Income by geographies
 # Cities, sale.dollar by income
@@ -96,16 +99,28 @@ ggplot(zipcodes.total.sales, aes(x = income, y = total.sales.volume/population))
   geom_point() +
   scale_y_log10()
 ```
+``` {r}
 ## Subcategory
 # Color by subcategory
-# filter by subcategory
-
-ggplot(cities, aes(x = income, y = sale.dollars)) +
-  geom_point() +
-  scale_y_log10()
 ggplot(cities, aes(x = income, y = sale.dollars, color = category)) +
   geom_point() +
   scale_y_log10()
+ggplot(counties, aes(x = income, y = sale.dollars, color = category)) +
+  geom_point() +
+  scale_y_log10()
+ggplot(zipcodes, aes(x = income, y = sale.dollars, color = category)) +
+  geom_point() +
+  scale_y_log10()
+# filter by subcategory
+counties_category <- counties %>%
+  filter(category == "Whisky")
+
+ggplot(counties_category, aes(x = income, y = sale.dollars)) +
+  geom_point() +
+  geom_smooth() +
+  scale_y_log10()
+```
+
 
 
 
